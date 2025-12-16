99247852d95ed5da780e1042da379618d06942f4
Sandro Filho <sandrofilho98@yahoo.com.br>
2025-12-15 23:37:09 -0300

    .

--- account/admin.py
+++ account/admin.py
@@ -3,7 +3,10 @@ from django.utils.translation import gettext_lazy as _
 from .models import User
 from django.contrib import admin, messages
 from asaas.models import AfiliadoAsaas, AsaasConfig
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 import requests
+from django.urls import reverse, resolve
 
 
 class AfiliadoAsaasInline(admin.StackedInline):
@@ -11,6 +14,54 @@ class AfiliadoAsaasInline(admin.StackedInline):
     extra = 0
     max_num = 1
     can_delete = False
+
+    readonly_fields = ["status_badge", "delete_link"]
+    # Link de exclusão
+    def delete_link(self, instance):
+        if not instance or not instance.pk:
+            return ""
+        url = reverse("admin:asaas_asaasconfig_delete", args=[instance.pk])
+        return mark_safe(
+            f'<a href="{url}" '
+            f'style="display: block; background: var(--delete-button-bg); '
+            f"border-radius: 4px; padding: 0.625rem 0.9375rem; "
+            f'line-height: 0.9375rem; color: var(--button-fg);">'
+            f"Excluir integração Asaas</a>"
+        )
+
+    delete_link.short_description = "Excluir"
+
+    # Badge de status
+    def status_badge(self, instance):
+        if not instance or not instance.status:
+            return "-"
+
+        css_class = "badge badge-" + instance.status.replace("_", "-").replace(
+            "ã", "a"
+        ).replace("í", "i")
+
+        return format_html(
+            '<span class="{}">{}</span>', css_class, instance.get_status_display()
+        )
+
+    status_badge.short_description = "Status"
+
+    # Campos exibidos
+    def get_fields(self, request, obj=None):
+        if not obj:
+            return ["token_api"]
+
+        instance = obj.asaas_config.first()
+
+        base = ["status_badge", "externalId", "delete_link"]
+
+        if instance:
+            if instance.status == "conectado":
+                return base
+            else:
+                return ["token_api"] + base
+
+        return ["token_api"]
 
     def has_add_permission(self, request, obj):
         # Só permite adicionar se não existir ainda
